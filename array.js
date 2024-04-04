7870aecad184674b9f9aee76f52733f900d15894
khushi panwar <125784585+khushi-panwar@users.noreply.github.com>
2024-04-04 15:10:19 +0530

    time complexity and space complexity

--- array.js
+++ array.js
@@ -264,7 +264,7 @@ function removeDuplicates(nums) {
 function removeDuplicateNew(nums) {
     if (nums.length === 0) return 0;
     let i = 0;
-
+    
     for (let j = 1; j < nums.length; j++) {
         if (nums[i] !== nums[j]) {
             i++;
@@ -276,3 +276,4 @@ function removeDuplicateNew(nums) {
 
 
 console.log(removeDuplicateNew([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]));
+// time complexity  : O(n), space complexity :  O(1)
